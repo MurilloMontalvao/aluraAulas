57894b19fea2e8044af8fca6d5392082a8b35244
MurilloMontalvao <limoncoding@gmail.com>
2025-02-22 12:42:49 -0300

    update

---
+++ Desafios/Desafios/004 - Array/D001 copy/app.js
@@ -0,0 +1,5 @@
+let listaDeProgramação = ['JavaScript','C','C++','Kotlin','Python'];
+listaDeProgramação.push = 'Java';
+listaDeProgramação.push =  'Ruby';
+listaDeProgramação.push =  'GoLang';
+console.log(listaDeProgramação);
